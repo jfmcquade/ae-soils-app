6c309aad225c89eade254f8956f3b59e75a754e1
Johnny McQuade <jfmcquade@hotmail.com>
2022-09-09 13:33:13 +0100

    added 'wash' deployment

---
+++ .idems_app/deployments/wash/config.ts
@@ -0,0 +1,20 @@
+import { IDeploymentConfig } from "data-models";
+import { getDefaultAppConstants } from "data-models/constants";
+
+const app_constants = getDefaultAppConstants();
+
+const config: IDeploymentConfig = {
+  name: "wash",
+  app_constants,
+  google_drive: {
+    sheets_folder_id: "1L-9KCMDB-ndNcj8Jp10mBX6ENkC9EcY6",
+    assets_folder_id: "1JrorEwpf4ItFvkVF5KqWhlBnN_5I4LVl",
+  },
+};
+
+config.app_constants.APP_SIDEMENU_DEFAULTS.title = "WASH App";
+config.app_constants.APP_HEADER_DEFAULTS.title = "WASH App";
+config.app_constants.NOTIFICATION_DEFAULTS.title = "New message from WASH App";
+config.app_constants.NOTIFICATION_DEFAULTS.text = "You have a new message from WASH App";
+
+export default config;
